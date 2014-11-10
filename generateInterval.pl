cbc343fee2b61ade9bd0b1228055b621d4b93a2c
Simon Renny-Byfield <simonrenny-byfield@Jeffreys-iMac.local>
2014-11-10 14:53:02 -0800

    created a file the outputs BED formatted intervals based on pattern match in a DNA string.

--- generateInterval.pl
+++ generateInterval.pl
@@ -12,19 +12,45 @@ use Bio::SeqIO;
 #This script takes fasta files and splits the sequences into individual files
 
 #Initialize the Bio:SeqIO object; 
-#my $seqIO = Bio::SeqIO -> new ( -format => "fasta" , -file => $ARGV[0] );
+my $seqIO = Bio::SeqIO -> new ( -format => "fasta" , -file => $ARGV[0] );
 
-my $str = "TGTGTGTGTGNNNNNNNNNNNNNNNNNNNTTTTNNNNTTTNNTTT";
+#open an output file
+open( OUT , ">$ARGV[0]".".txt" ) || die "Could not open file:$!\n";
+
+my @posi;
+
+#my $str = "TGTGTGTGTGNNNNNNNNNNNNNNNNNNNTTTTNNNNTTTNNTTT";
 my $substr = "N";
 #start looping through the sequences
-#while ( my $seqObj = $seqIO->next_seq() ) {
-my $pos = -1;	
-#my $str = $seqObj->seq();
+while ( my $seqObj = $seqIO->next_seq() ) {
+my $pos = -1;
+my $name = $seqObj->id;	
+my $str = $seqObj->seq();
   while (1) {
   	$pos = index($str, $substr, $pos + 1);
   	last if $pos < 0;
-  	print $pos . "\n";
-  }#while	
-#}#while
+  	push ( @posi , $pos  );
+  	#print $range[$#range] , "\n";
+  }#while
+  my $ranges = &num2range(@posi);
+  #print $ranges , "\n";
+  my @range = split /,/ , $ranges;
+  foreach (@range) {
+  	my @size = split /-/, $_;
+  	my $len = $size[1] - $size[0];
+  	#print $size[0] ,"\t" , $size[1] ,"\t" , $len , "\n";
+  	if ( $len >= 100 ) {
+  		print OUT "$name\t$_\n";
+  	}#if
+  }#foreach
+  @posi=();
+}#while
 
 exit;
+
+
+sub num2range {
+  local $_ = join ',' => sort { $a <=> $b } @_;
+  s/(?<!\d)(\d+)(?:,((??{$++1})))+(?!\d)/$1-$+/g;
+  return $_;
+}
